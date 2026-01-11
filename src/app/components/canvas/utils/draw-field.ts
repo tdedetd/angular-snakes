42f3e8f8a93fe4f8200128aaba29d6853918d447
tdedetd <tdedetd@gmail.com>
2026-01-11 18:47:47 +0300

    render optimization

--- src/app/components/canvas/utils/draw-field.ts
+++ src/app/components/canvas/utils/draw-field.ts
@@ -1,8 +1,12 @@
 import { Ai } from '../../../game/ai';
 import { Game } from '../../../game/game';
+import { Player } from '../../../game/models/player.model';
+import { Point } from '../../../game/models/point.model';
 import { toRgb } from '../../../utils/to-rgb';
 
 const cellLength = 18;
+const halfCellLength = cellLength / 2;
+
 const borderWidth = 1;
 const doubleBorderWidth = borderWidth * 2;
 const aiPathDebug = false;
@@ -14,6 +18,17 @@ export function drawField(
   ais: Ai[],
 ): void {
   ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+  drawBounds(ctx, game);
+  drawSnakes(ctx, game.remainingPlayers);
+  drawApples(ctx, game.apples);
+
+  if (aiPathDebug) {
+    drawAiPaths(ctx, ais);
+  }
+}
+
+function drawBounds(ctx: CanvasRenderingContext2D, game: Game): void {
   ctx.strokeStyle = 'white';
   ctx.lineWidth = borderWidth;
   ctx.strokeRect(
@@ -22,29 +37,45 @@ export function drawField(
     game.width * cellLength + doubleBorderWidth,
     game.height * cellLength + doubleBorderWidth
   );
+}
 
-  game.remainingPlayers.forEach(({ snake }) => {
+function drawSnakes(ctx: CanvasRenderingContext2D, players: Player[]): void {
+  players.forEach(({ snake }) => {
     ctx.fillStyle = snake.color;
+    ctx.beginPath();
     snake.parts.forEach(
-      (part) => ctx.fillRect(part.x * cellLength, part.y * cellLength, cellLength, cellLength)
+      (part) => ctx.rect(part.x * cellLength, part.y * cellLength, cellLength, cellLength)
+    );
+    ctx.fill();
+  });
+}
+
+function drawApples(ctx: CanvasRenderingContext2D, apples: Point[]): void {
+  ctx.fillStyle = '#87e47f';
+  ctx.beginPath();
+  apples.forEach((apple) => {
+    ctx.arc(
+      apple.x * cellLength + halfCellLength,
+      apple.y * cellLength + halfCellLength,
+      cellLength / 2,
+      0,
+      2 * Math.PI
     );
   });
+  ctx.fill();
+}
 
-  ctx.fillStyle = '#87e47f';
-  game.apples.forEach((apple) => {
-    ctx.fillRect(apple.x * cellLength, apple.y * cellLength, cellLength, cellLength);
+function drawAiPaths(ctx: CanvasRenderingContext2D, ais: Ai[]): void {
+  ais.filter(({ isOut }) => !isOut).forEach((ai) => {
+    const path = ai.getPath();
+    if (path) {
+      const rgb = toRgb(ai.color);
+      ctx.fillStyle = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.2)`;
+      ctx.beginPath();
+      path.forEach((point) => {
+        ctx.rect(point.x * cellLength, point.y * cellLength, cellLength, cellLength);
+      });
+      ctx.fill();
+    }
   });
-
-  if (aiPathDebug) {
-    ais.filter(({ isOut }) => !isOut).forEach((ai) => {
-      const path = ai.getPath();
-      if (path) {
-        const rgb = toRgb(ai.color);
-        ctx.fillStyle = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.2)`;
-        path.forEach((point) => {
-          ctx.fillRect(point.x * cellLength, point.y * cellLength, cellLength, cellLength);
-        });
-      }
-    });
-  }
 }
